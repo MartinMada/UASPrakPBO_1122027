1500ebd5663629a50f871b1821001babe21a120b
martin <martin@MSI>
2023-11-17 12:05:55 +0700

    1122027_MartinusMada_UAS

---
+++ src/uasprakpbo/GUI.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uasprakpbo;
+
+/**
+ *
+ * @author marti
+ */
+public class GUI {
+    
+    static DatabaseHandler conn = new DatabaseHandler();
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
